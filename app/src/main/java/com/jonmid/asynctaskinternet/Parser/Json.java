88fddf0b00ed9bfdcaad522f430cd1a257a9fbc0
Luis Rosales <luchorosalesc6@gmail.com>
2017-09-21 13:07:26 -0500

    Initial commit

---
+++ app/src/main/java/com/jonmid/asynctaskinternet/Parser/Json.java
@@ -0,0 +1,33 @@
+package com.jonmid.asynctaskinternet.Parser;
+
+import com.jonmid.asynctaskinternet.Models.Comment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by aula7 on 21/09/17.
+ */
+
+public class Json {
+    public static List<Comment> getData(String content) throws JSONException {
+        //convierte una cadena en un Json de array
+        JSONArray jsonArray=new JSONArray(content);
+        List<Comment> commentList =new ArrayList<>();
+        for (int i = 0; i<jsonArray.length();i++){
+            JSONObject item=jsonArray.getJSONObject(i);
+
+            Comment comment=new Comment();
+            comment.setId(item.getInt("id"));
+            comment.setEmail(item.getString("email"));
+            comment.setBody(item.getString("body"));
+            commentList.add(comment);
+        }
+        return commentList;
+    }
+
+}
