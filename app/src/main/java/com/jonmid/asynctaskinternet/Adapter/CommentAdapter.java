bba2a1f97b1413f8cf5bbd677c93fd7d1646d5af
Luis Rosales <luchorosalesc6@gmail.com>
2017-09-21 13:41:01 -0500

    Guardar 3

--- app/src/main/java/com/jonmid/asynctaskinternet/Adapter/CommentAdapter.java
+++ app/src/main/java/com/jonmid/asynctaskinternet/Adapter/CommentAdapter.java
@@ -3,12 +3,15 @@ package com.jonmid.asynctaskinternet.Adapter;
 import android.content.Context;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jonmid.asynctaskinternet.Models.Comment;
+import com.jonmid.asynctaskinternet.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,12 +32,28 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHold
         this.context = context;
     }
 
-//
+
+
     @Override
+    //este metodo Nos permite configurar el viewholder
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+
+//Para inflar las vistas Layoutinflater
+//        para obtener el archivo item.xml
+        View item= LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+
+//        Permite pasar los componentes del archivo.xml para hacer referencia de ellos.
+        ViewHolder viewHolder=new ViewHolder(item);
         return null;
     }
 
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+//  encargado de trabajar los componentes del Item
+
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 //        toda clase adapter necesite un viewholder, es una clase que permite hacer referencia a los componentes
@@ -45,6 +64,9 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHold
 
         public ViewHolder(View item) {
             super(item);
+            imageView=(ImageView) item.findViewById(R.id.img_person);
+            textViewEmail=(TextView) item.findViewById(R.id.tv_email_1);
+            textViewBody=(TextView) item.findViewById(R.id.tv_body);
         }
     }
 }
